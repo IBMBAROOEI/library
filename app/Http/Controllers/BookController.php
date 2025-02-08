2cfb7dd1e36b51eacc36ac8a10e2a598f43d6ea1
ibmbarooei <ibmbarooei@gmail.com>
2025-02-08 03:13:21 -0800

    add file swagger

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+/**
+ * @OA\Info(title="My API", version="8.6")
+ */
+
 class BookController extends Controller
 {
-    //
+    
+
+     /**
+     * @OA\Get(
+     *     path="/api/books",
+     *     operationId="getbooks",
+     *     tags={"books"},
+     *     summary="Get list of books",
+     *     @OA\Response(response="200", description="List of books")
+     * )
+     */
+
+
+     public function index(){
+
+         return  response()->json("s",200);
+     }
 }
