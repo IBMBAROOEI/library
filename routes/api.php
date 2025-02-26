8f6d08964484bb8c21c63b8b8bab5274a86241af
ibmbarooei <ibmbarooei@gmail.com>
2025-02-26 02:36:32 -0800

    create file acvtion user

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BookController;
 
-Route::resource('books', BookController::class);  
+Route::resource('books', BookController::class);
+Route::resource('user', AuthController::class);
