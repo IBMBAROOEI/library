19d9c3a76556fd88587724d59b054245b2095a6f
ibmbarooei <ibmbarooei@gmail.com>
2025-03-14 21:23:02 +0330

    create trait

---
+++ app/HasRolesAndPermissions.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+
+
+trait HasRolesAndPermissions{
+
+
+
+
+        public function roles(): BelongsToMany
+        {
+            return $this->belongsToMany(Role::class);
+        }
+
+
+
+
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+
+
+    public function assignRole(...$role){
+
+        $role=is_array($role[0])?$role[0]:$role;
+        $this->roles()->sync($role,false);
+        return $this;
+    }
+
+    public function hasRole(string $role):bool{
+
+        return $this->roles()->wherIn('name',$role)->exists();
+    }
+
+
+    public function hasAnyRole(...$role):bool
+    {
+
+        $role = is_array($role[0]) ? $role[0] : $role;
+        return $this->roles()->wherIn('name', $role)->exists();
+    }
+
+
+    public function hasPermissionTo($permission){
+        return $this->permissions()->where('name', $permission);
+    }
+
+}
