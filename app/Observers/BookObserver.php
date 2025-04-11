148313af35b56a97aaaba4ac32df0d80504eafc7
ibmbarooei <ibmbarooei@gmail.com>
2025-04-11 15:37:19 +0330

    observer_book

---
+++ app/Observers/BookObserver.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Book;
+
+class BookObserver
+{
+    /**
+     * Handle the Book "created" event.
+     */
+    public function created(Book $book): void
+    {
+        IndexBook::dispatch($book);
+    }
+
+    /**
+     * Handle the Book "updated" event.
+     */
+    public function updated(Book $book): void
+    {
+        IndexBook::dispatch($book);
+    }
+
+    /**
+     * Handle the Book "deleted" event.
+     */
+    public function deleted(Book $book): void
+    {
+        DeleteBookFromIndex::dispatch($book->id);
+    }
+
+
+}
